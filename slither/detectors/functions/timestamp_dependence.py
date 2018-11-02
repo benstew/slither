90fd388105b00e943c6c6c98482b0c8e069c4835
benstew <benstewart2008@gmail.com>
2018-11-02 18:12:31 -0400

    stub out some info

--- slither/detectors/functions/timestamp_dependence.py
+++ slither/detectors/functions/timestamp_dependence.py
@@ -21,11 +21,12 @@ class UnusedStateVars(AbstractDetector):
         # Get all the variables read in all the functions and modifiers
         variables_used = [x.state_variables_read + x.state_variables_written for x in
                           (contract.all_functions_called + contract.modifiers)]
+
         # Flat list
         variables_used = [item for sublist in variables_used for item in sublist]
-        # Return the variables unused that are not public
-        return [x for x in contract.variables if
-                x not in variables_used and x.visibility != 'public']
+
+        # Return timestamp dependencies
+        
 
     def detect(self):
         """ Detect timestamp dependence
